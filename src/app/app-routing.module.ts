1926b6e8e974fa1b155760420f01b1d2f59e5801
Bakraam <bakraam13@gmail.com>
2023-08-17 11:44:50 +0100

    Creación de rutas
    Creación de rutas en app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"newuser", component: NewUserComponent},
+  {path:"user/:iduser", component: UserViewComponent},
+  {path:"updateuser/:iduser", component: UpdateUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
