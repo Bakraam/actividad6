1c00debfb82b504211e09e35d63cf588f4dc2572
Bakraam <bakraam13@gmail.com>
2023-08-17 11:27:47 +0100

    Creación de pages
    Creación de las páginas home, userView, newUser y updateUser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserViewComponent,
+    NewUserComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
