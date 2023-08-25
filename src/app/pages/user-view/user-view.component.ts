bd4733a7f7d065f713a02383124e38d65012153d
Bakraam <bakraam13@gmail.com>
2023-08-25 16:51:45 +0100

    Creación del componente user-view
    Se maqueta el html, se le añade estilo con css y funcionalidad con el ts

--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-user-view',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-view.component.css']
 })
 export class UserViewComponent {
+  
+  activatedRoute = inject(ActivatedRoute);
+  oneUser!: User | any;
+  usersService = inject(UsersService);
+  router = inject(Router)
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(async (params:any)=>{
+      let id = params.iduser;
+      this.oneUser = await this. usersService.getById(id);
+    })
+  }
+
+  async deleteUser(_id:string): Promise<void> {
+    alert ('Se va a eliminar este usuario. ¿Deseas continuar?')
+    let response = await this.usersService.delete(_id);
+    console.log(response);
+    if (response) {
+      alert ('El usuario ha sido eliminado correctamente')
+      this.router.navigate(['/home'])
+    }
+
+  }
 
 }
