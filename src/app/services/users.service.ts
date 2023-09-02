dc0a430bb7440ec776805d3522333c7e5e896c54
Bakraam <bakraam13@gmail.com>
2023-09-02 11:21:09 +0100

    Método getAll2 para page 2
    Se añade un nuevo método para poder obtener los usuarios de la página 2

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -10,6 +10,7 @@ export class UsersService {
 
   httpClient = inject(HttpClient)
   private baseUrl: string = "https://peticiones.online/api/users/";
+  private baseUrl2: string = "https://peticiones.online/api/users?page=2 "
 
   constructor() { }
 
@@ -18,23 +19,28 @@ export class UsersService {
     return this.httpClient.get<any>(this.baseUrl)
   }
 
+  //Obtener users page 2
+  getAll2(): Observable<any>{
+    return this.httpClient.get<any>(this.baseUrl2)
+  }
+
   //Obtener user por su id
   getById(id: string) : Promise<User> {
-    return lastValueFrom(this.httpClient.get<User>(`${this.baseUrl}${id}`))
+    return lastValueFrom(this.httpClient.get<User>(`${this.baseUrl}${id}`))  //id o _id
   }
 
   //Borrar user
   delete(id:string) : Promise<any> {
-    return lastValueFrom(this.httpClient.delete<any>(`${this.baseUrl}${id}`))
+    return lastValueFrom(this.httpClient.delete<any>(`${this.baseUrl}${id}`))  //id o _id
   }
 
   //Insertar nuevo user
-  /*
-  newUser(formValue:any) : Promise<any> {
-    return lastValueFrom(this.httpClient.post<any>(`${this.baseUrl}${id}`))
+  insert(formValue:any) : Promise<User> {
+    return lastValueFrom(this.httpClient.post<User>(this.baseUrl,formValue))
   }
-  */
-
+  
   //Actualizar user
-
+  update(formValue:User): Promise <User>{
+    return lastValueFrom(this.httpClient.put<User>(`${this.baseUrl}${formValue._id}`,formValue))
+  }
 }
